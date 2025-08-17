ec88248fd5a5d7999d9d683d3a9b22cecb41feaa
kashishrajputt <btech41522029@dseu.ac.in>
2025-08-17 14:04:45 +0530

    functions

--- functions.php
+++ functions.php
@@ -14,6 +14,30 @@
      }
      sum(10,60);
 
+     //varargs
+     function sums(...$a){
+        $sum = 0;
+        foreach($a as $m){
+            echo "<br>".$m."<br>";
+            $sum = $sum + $m;
+
+        }
+        echo "sum is: ".$sum;
+     }
+
+     sums(10,20,30,40,900);
+
+     function factorial($n){
+        $f = 1;
+        while($n!=0){
+            $f = $f* $n;
+            $n--;      
+        }
+        return $f;
+     }
+     $m = factorial(5);
+     echo "<br>Factorial is: ".$m;
+     
      ?>
 </body>
 </html>
